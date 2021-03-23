c5e36e4618795e781ee143b75fd4fcf3a8a3bf74
Luis Manzo <manzosk3@gmail.com>
2021-03-22 21:29:11 -0400

    cleans up useless components and styles

--- src/App.vue
+++ src/App.vue
@@ -4,23 +4,19 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  font-family: sans-serif;
+  padding-top: 1em;
+  padding-bottom: 1em;
+  font-size: 2em;
+  width: 80%;
+  margin: 0 auto;
 }
 </style>
